b8768f9ce7a40de310386000b45f91770a6c0d49
Zack Pashkin <kaisenaiko@gmail.com>
2021-07-16 23:34:36 +0700

    fixed

--- model.py
+++ model.py
@@ -9,11 +9,11 @@ class Linear_Qnet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.out = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
-        x = self.linear2(x)
+        x = self.out(x)
         return x
 
     def save(self, file_name='model.pth'):
